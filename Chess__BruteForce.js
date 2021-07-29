98b0776f1ffcc9b3d1bee00a13bc4ecce55fe6fc
totobiyo <chap95@likelion.org>
2021-07-30 07:50:30 +0900

    add chess test case

--- Chess__BruteForce.js
+++ Chess__BruteForce.js
@@ -1,37 +1,43 @@
-const readline = require('readline');
+const readline = require("readline");
+var fs = require("fs");
+var input = fs.readFileSync("chess.txt").toString().split("\n");
+solve(input);
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// console.log("input->", input);
+// let input = [];
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// rl.on("line", function (line) {
+//   input.push(line);
+// }).on("close", function () {
+//   solve(input);
 
-let input = [];
-
-rl.on('line', function (line) {
-  input.push(line)
-}).on('close', function() {
-  solve(input);
-
-  process.exit();
-})
-
+//   process.exit();
+// });
 
 function solve(input) {
   var aspect = input[0].split(" ");
-  var horizontal = aspect[0];
-  var vertical = aspect[1];
+  var horizontal = Number(aspect[0]);
+  var vertical = Number(aspect[1]);
   var allInOneRow = [];
-  
-  for(var i = 1; i + 7 <= horizontal; i++ ){
-    console.log(i)
-    for(var j = 0; j + 8 < vertical; j++){
-      for(var h = i; h < h+8; h++){
+  console.log("horizontal , vertical -> ", horizontal, vertical);
+  for (var i = 1; i + 7 <= horizontal; i++) {
+    console.log("candidate index -> ", i);
+    for (var j = 0; j + 7 < vertical; j++) {
+      for (var h = i; h < i + 8; h++) {
+        console.log("selectd index -> ", h);
         var row = input[h].split("");
-        console.log('row -> ', row);
-        for(var v  = j; v < v+8; v++){
-
+        console.log("row -> ", row);
+        for (var v = j; v < j + 8; v++) {
+          allInOneRow.push(row[v]);
         }
       }
     }
   }
+
+  console.log(allInOneRow);
+
+  return;
 }
