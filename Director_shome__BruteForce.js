facefc49fc25ea53fbb920ad8865b7f0900ce884
chap95 <chap95@likelion.org>
2021-08-02 21:40:04 +0900

    add directore shome

--- Director_shome__BruteForce.js
+++ Director_shome__BruteForce.js
@@ -6,50 +6,25 @@ const rl = readline.createInterface({
 });
 let input = [];
 
-let counter;
-
 rl.on("line", function (line) {
   input.push(line);
 }).on("close", function () {
-  solve(input[0]);
+  var result = solve(input[0]);
+  console.log(result);
 });
 
 function solve(value){
-  var origin = parseInt(value);
-  let prefixNumber = origin - 1;
-  var rawNumber = prefixNumber * 1000 + 666;
-  var stringNumber = String(rawNumber);
-  var prefixArray = String(prefixNumber).split('');
-  prefixArray.map((value) => {
-    if(value === '6'){
-      
-    }
-  })
+  var input = parseInt(value);
+  var leastApocalypseNumber = 665;
 
-
-  console.log(stringNumber.split("").reduce(reducer));
-  function reducer(accumulator, currentValue, currentIndex){
-    
-    if(currentIndex === 1){
-      counter = accumulator === '6' ? 1 : 0;
+  while(input > 0){
+    leastApocalypseNumber++;
+    if(leastApocalypseNumber.toString().indexOf('666') !== -1){
+      input--
     }
-  
-    if(currentValue === '6'){
-      counter++;
-    } else {
-      counter = 0
-    }
-  
-    console.log(currentIndex, counter)
-    
-    if(counter > 3){
-      return accumulator + "0";
-    }
-  
-  
-    
-    return accumulator + currentValue;
   }
+
+  return leastApocalypseNumber
 }
 
 
